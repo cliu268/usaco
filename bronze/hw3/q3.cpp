753f225cfdada8ab9e295b0b4e3c4469d9ddb7d0
cliu268 <cliu268@gmail.com>
2022-01-27 01:30:42 -0500

    hw3

---
+++ bronze/hw3/q3.cpp
@@ -0,0 +1,6 @@
+// USACO 2015 December Contest, Bronze
+// Problem 1. Fence Painting
+// http://www.usaco.org/index.php?page=viewproblem2&cpid=567
+/*
+https://github.com/cliu268/usaco/blob/main/bronze/mock/q0-1.cpp
+*/
